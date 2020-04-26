f730d96d2fb59e2bf22b42d3968d4d93bbaa1983
Michel Gomes da Silva <michel@uconex.com.br>
2020-04-26 16:22:51 +0100

    Singup Created

--- lib/pages/signup-page.dart
+++ lib/pages/signup-page.dart
@@ -1,14 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/widgets/signup-textfields-widget.dart';
 
 class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Signup"),
+      appBar: AppBar(
+        backgroundColor: Colors.white.withOpacity(0),
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          color: Color(0xFFF5F5F5),
+          padding: EdgeInsets.only(
+            top: 80,
+            left: 20,
+            right: 20,
+            bottom: 40,
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 450,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black12,
+                      offset: new Offset(1, 2.0),
+                      blurRadius: 5,
+                      spreadRadius: 1,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 15, right: 15, top: 60),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "Signup",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SignUpTextField(),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                        child: FlatButton(
+                          child: Text(
+                            "Signup",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
